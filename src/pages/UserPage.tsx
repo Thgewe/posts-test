9429928b6bd6e08aacc4e4641d970901d934fff1
Viktor Khankhatov <hanhatov@mail.ru>
2023-06-03 21:06:22 +0800

    new: userPage back button

--- src/pages/UserPage.tsx
+++ src/pages/UserPage.tsx
@@ -1,15 +1,16 @@
 import React, {useEffect} from 'react';
 import UserInfo from "../components/UserInfo";
 import PostsList from "../components/PostsList";
-import {Container} from "react-bootstrap";
+import {Button, Container} from "react-bootstrap";
 import {useAppDispatch} from "../hooks/redux";
 import {fetchUser} from "../store/reducers/userReducer";
-import {useParams} from "react-router-dom";
+import {useNavigate, useParams} from "react-router-dom";
 
 const UserPage = () => {
 
     const dispatch = useAppDispatch();
     const params = useParams();
+    const navigate = useNavigate();
 
     useEffect(() => {
         dispatch(fetchUser(params.id !== undefined ? params.id : '1'));
@@ -19,6 +20,16 @@ const UserPage = () => {
         <Container>
             <UserInfo />
             <PostsList userId={params.id && params.id}/>
+            <Button
+                className={'position-fixed fixed-bottom ms-auto me-2 mb-2'}
+                variant={'warning'}
+                style={{
+                    width: 'fit-content',
+                }}
+                onClick={() => navigate(-1)}
+            >
+                Назад
+            </Button>
         </Container>
     );
 };
